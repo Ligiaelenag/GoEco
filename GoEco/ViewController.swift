a5b075fd3be9d05aba97d838b7a923a3d0198e7c
Ligia Elena <Ligiaelenag12@gmail.com>
2020-08-12 22:45:43 -0700

    Beginning

---
+++ GoEco/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoEco
+//
+//  Created by ligia Elena Guevara on 8/12/20.
+//  Copyright © 2020 Ligia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
